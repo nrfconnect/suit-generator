0bba34b5e154f41c398940ea6bd0e1abf7b32970
Adam Szczygieł <adam.szczygiel@nordicsemi.no>
2023-04-06 14:02:05 +0200

    Add a new command - convert
    A new command to convert private key in PEM format into a C array with
    public key data.
    
    Signed-off-by: Adam Szczygieł <adam.szczygiel@nordicsemi.no>

--- suit_generator/args.py
+++ suit_generator/args.py
@@ -10,10 +10,12 @@ from argparse import ArgumentParser
 from typing import Tuple
 
 from suit_generator.cmd_keys import KeyGenerator
+from suit_generator.cmd_convert import KeyConverter
 
 PARSE_CMD = "parse"
 CREATE_CMD = "create"
 KEYS_CMD = "keys"
+CONVERT_CMD = "convert"
 SIGN_CMD = "sign"
 
 
@@ -89,8 +91,81 @@ def parse_arguments() -> Tuple:
         help=f"Key encryption. Default: {KeyGenerator.default_encryption}",
         choices=KeyGenerator.supported_encryptions.keys(),
     )
+    # CONVERT_CMD command
+    cmd_convert_arg_parser = subparsers.add_parser(
+        CONVERT_CMD, help="Convert private key file in PEM format into a C file storing public key data as an array."
+    )
+    cmd_convert_arg_parser.add_argument("--input-file", required=True, help="Private key file in PEM format.")
+    cmd_convert_arg_parser.add_argument(
+        "--output-file", required=True, help="C file to be created; will hold public key data as an array."
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--array-type",
+        required=False,
+        default=KeyConverter.default_array_type,
+        help=f"C type to be used as a key contents array. Default: {KeyConverter.default_array_type}",
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--array-name",
+        required=False,
+        default=KeyConverter.default_array_name,
+        help=f"Valid C variable name to be used for array containing the key. "
+        f"Default: {KeyConverter.default_array_name}",
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--length-type",
+        required=False,
+        default=KeyConverter.default_length_type,
+        help=f"C type to be used as an array length variable. Default: {KeyConverter.default_length_type}",
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--length-name",
+        required=False,
+        default=KeyConverter.default_length_name,
+        help=f"Valid C variable name to be used for array length. Default: {KeyConverter.default_length_name}",
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--columns-count",
+        required=False,
+        type=int,
+        default=KeyConverter.default_columns_count,
+        help=f"Number of columns of C code of an array. Default: {KeyConverter.default_columns_count}",
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--header-file",
+        required=False,
+        default=KeyConverter.default_header_file,
+        help=f"Use this file's contents as a generated C file header/banner, e.g. as a license, #ifdef guards, etc. "
+        f"Default: {KeyConverter.default_header_file}",
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--footer-file",
+        required=False,
+        default=KeyConverter.default_footer_file,
+        help=f"Use this file's contents as a generated C file footer, e.g. to close #ifdef guards. "
+        f"Default: {KeyConverter.default_footer_file}",
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--indentation-count",
+        required=False,
+        default=KeyConverter.default_indentation_count,
+        help=f"Number of indentation characters to put at the beginning of array lines. "
+        f"Default: {KeyConverter.default_indentation_count}",
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--indentation-tab",
+        required=False,
+        action="store_true",
+        help="Use tab instead of space as indentation character.",
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--no-length", required=False, action="store_true", help="Do not create array length variable."
+    )
+    cmd_convert_arg_parser.add_argument(
+        "--no-const", required=False, action="store_true", help="Do not use 'const' modifier for variables."
+    )
     # SIGN_CMD command
-    cmd_sign_arg_parser = subparsers.add_parser(SIGN_CMD, help="Sign manifest")
+    cmd_sign_arg_parser = subparsers.add_parser(SIGN_CMD, help="Sign manifest.")
     cmd_sign_arg_parser.add_argument("--input-file", required=True, help="Input SUIT file")
     cmd_sign_arg_parser.add_argument("--output-file", required=True, help="Output SUIT file")
     cmd_sign_arg_parser.add_argument("--private-key", required=True, help="Private key file")
