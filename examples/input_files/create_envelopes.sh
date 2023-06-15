99640ecbecea088e72d8f6bbc6f78e95304c7b45
Robert Stypa <robert.stypa@nordicsemi.no>
2023-06-15 08:08:28 +0200

    feat (licenses): add missing licenses in the sh scripts.
    Add missing licenses in the sh scripts.
    
    Signed-off-by: Robert Stypa <robert.stypa@nordicsemi.no>

--- examples/input_files/create_envelopes.sh
+++ examples/input_files/create_envelopes.sh
@@ -1,3 +1,8 @@
+#
+# Copyright (c) 2023 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+#
 #!/usr/bin/env sh
 # Create two basic envelopes containing dummy payloads.
 dd if=/dev/zero of=file.bin bs=1024 count=1
