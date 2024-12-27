645ceb64fbf50efb6a69118bc615f137f395fcc7
Artur Hadasz <artur.hadasz@nordicsemi.no>
2024-12-27 11:36:53 +0100

    New version of signing script and signing process
    Ref: NCSDK-31069
    
    Signed-off-by: Artur Hadasz <artur.hadasz@nordicsemi.no>

--- suit_generator/args.py
+++ suit_generator/args.py
@@ -17,6 +17,7 @@ from suit_generator.cmd_convert import add_arguments as convert_args
 from suit_generator.cmd_mpi import add_arguments as mpi_args
 from suit_generator.cmd_cache_create import add_arguments as cache_create_args
 from suit_generator.cmd_payload_extract import add_arguments as payload_extract_args
+from suit_generator.cmd_sign import add_arguments as sign_args
 
 
 def _parser() -> ArgumentParser:
@@ -32,6 +33,7 @@ def _parser() -> ArgumentParser:
     mpi_args(subparsers)
     cache_create_args(subparsers)
     payload_extract_args(subparsers)
+    sign_args(subparsers)
     return parser
 
 
