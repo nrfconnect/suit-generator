9537eab87b4b45d25e98328ae015c064c0952c3a
Artur Hadasz <artur.hadasz@nordicsemi.no>
2025-01-16 10:59:41 +0100

    suit: Add support for Ed25519PH
    Ref: NCSDK-31353
    
    Signed-off-by: Artur Hadasz <artur.hadasz@nordicsemi.no>

--- suit_generator/suit/types/keys.py
+++ suit_generator/suit/types/keys.py
@@ -694,6 +694,13 @@ class cose_alg_eddsa(suit_key):
     name = "cose-alg-eddsa"
 
 
+class cose_alg_vs_hashed_eddsa(suit_key):
+    """Cose algorithm metadata."""
+
+    id = -65537
+    name = "cose-alg-vs-hashed-eddsa"
+
+
 class cose_alg_aes_gcm_128(suit_key):
     """Cose algorithm metadata."""
 
